bd731dd6f99fad87d32e02a76d4a3879773b6114
sowjanya0613 <>
2022-02-02 09:17:24 +0000

    executed

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(int *pa,int *pb)
+{
+  printf("enter two numbers\n");
+  scanf("%d%d",pa,pb);
 }
+void add (int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("%d+%d=%d\n",a,b,sum);
+}
+int main()
+{
+  int a,b,c;
+  input(&a,&b);
+  add(a,b,&c);
+  output(a,b,c);
+  return 0;
+  }
